6599e2476fad5643b69499dc944a99ad8ec2b636
wallarug <wallarug@bitbucket.org>
2019-01-27 18:35:55 +1100

    added test for robotics masters mm1 using cpy

---
+++ examples/cricketservos/cricketservos.ino
@@ -0,0 +1,44 @@
+#include "Adafruit_Crickit.h"
+#include "seesaw_servo.h"
+
+Adafruit_Crickit crickit;
+
+#define NUM_SERVOS 3
+
+seesaw_Servo servos[] = { seesaw_Servo(&crickit), 
+                          seesaw_Servo(&crickit), 
+                          seesaw_Servo(&crickit) };
+
+//these are the pins they will be attached to
+int servoPins[] = { CRICKIT_SERVO1, CRICKIT_SERVO2, CRICKIT_SERVO3 };
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+
+  //begin the crickit
+  if(!crickit.begin()){
+    Serial.println("ERROR!");
+    while(1);
+  }
+  else Serial.println("Crickit started");
+  
+  //attach the servos to their pins
+  for(int i=0; i<NUM_SERVOS; i++)
+    servos[i].attach(servoPins[i]);  // attaches the servo to the pin
+}
+ 
+// the loop function runs over and over again forever
+void loop() {
+  //repeat for all 4 servos
+  for(int i=0; i<NUM_SERVOS; i++){
+    servos[i].write(0);
+    delay(1000);
+    servos[i].write(90);
+    delay(1000);
+    servos[i].write(180);
+    delay(1000);
+    servos[i].write(90);
+    delay(1000);
+  }
+}
---
+++ examples/mm1-cpy-native-test.py
@@ -0,0 +1,7 @@
+# CircuitPython Test for Robotics Masters MM1
+#
+# Notes:
+#   This is to be run using CircuitPython 3.1
+#
+
+
